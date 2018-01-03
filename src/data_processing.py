022d5b2a9d49cb8b765d5619145d172b2eff8426
Lee Harper <leeharper2425@gmail.com>
2018-01-03 11:24:07 -0600

    Added a load data function and sketched out convert datetime function

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,3 +1,24 @@
 """
 A set of functions that will process that data so that it can be used for model building.
 """
+
+import pandas as pd
+
+def load_data(filename):
+    """
+    Load the data in json format.
+    :param filename: str, the relative path to a json file
+    :return: df, a pandas dataframe of the json file
+    """
+    return pd.read_json(filename)
+
+
+def convert_unix_to_datetime(df):
+    """
+    Convert all time columns from unix time to datetime.
+    :param df: pandas dataframe of the fraud detection data
+    :return: df2: pandas dataframe, with unix dates converted to datetimes
+    """
+    pass
+
+
